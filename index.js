c09ddca571690530ac6b95fd1f8e34c7a1f2a33a
whoozit.whatsit@gmail.com <whoozit.whatsit@gmail.com>
2018-07-13 17:47:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase();
 }
+
+function logShout(string) {
+  const upperCase = string.toUpperCase;
+  console.log(upperCase);
+}
